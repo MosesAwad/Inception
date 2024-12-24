2b40dae621028912e591ae917629a0fce13cb14f
Moosa <mawad@student.42abudhabi.ae>
2024-12-24 20:08:37 +0400

    modified the contents of the nginx directory as it previously had bugs which made it unbuildable

--- requirements/nginx/tools/script.sh
+++ requirements/nginx/tools/script.sh
@@ -4,8 +4,8 @@ CERTS=/etc/ssl/certs/mawad_selfsigned_cert.crt
 
 openssl req -x509 -nodes -days 365 \
     -newkey rsa:2048 \
-    -keyout /etc/ssl/private//mawad_selfsigned_private_key\
+    -keyout /etc/ssl/private/mawad_selfsigned_private.key \
     -out $CERTS  \
-    -subj "C=AE/L=Abu Dhabi/O=42AD/CN=mawad.42.fr"
+    -subj "/C=AE/L=Abu Dhabi/O=42AD/CN=mawad.42.fr"
 
 exec "$@"
